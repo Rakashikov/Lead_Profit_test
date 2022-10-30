56a8f763c4305038c204beded7f6ec5b083dbea0
Evgeniy Rakashikov <evgeniyhd@gmail.com>
2022-10-30 17:56:52 +0600

    Fix code

--- Task 3/findfiles.php
+++ Task 3/findfiles.php
@@ -1,30 +1,4 @@
 <?php
-// /**
-//  * getFiles
-//  * 
-//  * Возвращает массив имен файлов в директории
-//  *
-//  * @return array $files
-//  */
-// function getFiles()
-// {
-//     $path = 'Task 3/datafiles/';
-//     $regex = '/^[a-zA-Z0-9]+\.ixt/';
-//     $files = array();
-//     $dir = opendir($path);
-//     while ($file = readdir($dir)) {
-//         if (preg_match($regex, $file)) {
-//             $files[] = $file;
-//         }
-//     }
-//     sort($files);
-//     print_r($files);
-//     closedir($dir);
-//     return $files;
-// }
-
-// getFiles();
-
 /**
  * getFiles
  * 
@@ -49,6 +23,6 @@ function getFiles($path, $regex)
 }
 
 $path = 'Task 3/datafiles/';
-$regex = '/^[a-zA-Z0-9]+\.ixt/';
+$regex = '/^\w+\.ixt/';
 
 print_r(getFiles($path, $regex));
